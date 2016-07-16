3b86b5dbc4e4c6dc9abe18d1f93e17a195a23587
Adam Keys <akeys6803@gmail.com>
2016-07-15 22:27:02 -0400

    adds basic dialog implementation

---
+++ app/js/dialog.js
@@ -0,0 +1,43 @@
+const $ = require('jQuery');
+
+class Dialog {
+    constructor(options) {
+        this.dialog = $('.dialog').clone();
+        this.options = $.extend({
+            show: true,
+            animation_speed: 150,
+            starting_top: 10,
+            final_top: 40
+        }, options ? options : {});
+    }
+
+    show(dialogContent, options) {
+        if(!$('.glass').length)
+            $('body').append($('<div />', {class: 'glass'}));
+        let glass = $('.glass').fadeIn({duration: this.options.animation_speed, complete: () => {
+            $('.header .title', this.dialog).text($('.title', dialogContent).text());
+            $('.body', this.dialog).append($('.content', dialogContent).children());
+            $('.footer', this.dialog).append($('.footer', dialogContent).children());
+            $(this.dialog).click((event) => {event.stopPropagation();});
+            $('.glass').append(this.dialog.addClass('active').fadeIn(this.options.animation_speed).animate({top: this.options.final_top}, this.options.animation_speed).dequeue());
+            let close = () => {
+                if(this.options.close ? this.options.close($(this).data('intent')) : true)
+                    this.hide();
+            }
+            $('.footer > button', this.dialog).click(close);
+            $('.header .close', this.dialog).click(close);
+            $(glass).click(close);
+        }});
+    }
+
+    hide() {
+        let dialog = $('.dialog.active').fadeOut({duration: this.options.animation_speed, complete: () => {
+            $(dialog).remove();
+            let glass = $('.glass').fadeOut({duration: this.options.animation_speed, complete: () => {
+                $(glass).unbind('click');
+            }});
+        }}).animate({top: this.options.starting_top}, this.options.animation_speed).dequeue();
+    }
+}
+
+module.exports = Dialog;
--- app/js/pages.js
+++ app/js/pages.js
@@ -1,34 +1,8 @@
-require('jQuery');
-
-/*
-var clay = (function(clay, $) {
-    let _pages = {
-        show: function(page, tab) {
-            $('.content').children(':first').appendTo($('.pages'));
-            $(page).appendTo($('.content'));
-            $('.nav-item').removeClass('selected');
-            $(tab).addClass('selected');
-        }
-    };
-
-    let pages = {
-        register: function(tab, page, show) {
-            tab.click(function() {
-                _pages.show(page, this);
-            });
-            
-            if(show)
-                _pages.show(page, tab);
-            },
-        };
-        
-    return $.extend(clay, {pages: pages});
-}(window.clay = window.clay || {}, require('jQuery')));
-*/
+const $ = require('jQuery');
 
 function showPage(page, tab) {
-    $('.content').children(':first').appendTo($('.pages'));
-    $(page).appendTo($('.content'));
+    $('body > .content').children(':first').appendTo($('.pages'));
+    $(page).appendTo($('body > .content'));
     $('.nav-item').removeClass('selected');
     $(tab).addClass('selected');
 }
@@ -38,7 +12,7 @@ module.exports = {
         tab.click(function() {
             showPage(page, this);
         });
-        
+
         if(show)
             showPage(page, tab);
     }
