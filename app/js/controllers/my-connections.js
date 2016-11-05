628ee6a61b18faf9124ae2100bda459721ea556e
Adam Keys <akeys6803@gmail.com>
2016-11-04 23:33:15 -0400

    refs #3 Delete saved connections
    The ability to delete connections has been added to the My Connections page.
    An actionbar has been added to the footer which toggles connection rendering.
    Multiple connections can be selected for deletion while the actionbar is
    open.  Routes have been added to the main process to update application
    data based on selected connections.  Temporary css styles have been added
    for rendering connections while the actionbar is open and when connections have
    been selected for deletion.  These styles need to be cleaned up!

--- app/js/controllers/my-connections.js
+++ app/js/controllers/my-connections.js
@@ -16,14 +16,12 @@ function getTemplate(templates, id) {
     return found;
 }
 
-function renderConnection(connection, template) {
-    let connectionTemplate = $('.fresh-connection .connection').clone();
-    let port = connection.port;
-    let host = connection.host + (port && port != '22' ? ':' + port : '');
+function bindConnection(connection, connectionTemplate, template) {
+    $(connectionTemplate).click(() => {
+        console.log('launch');
+    });
 
     $(connectionTemplate).css('background-color', template.background);
-    $('.name', connectionTemplate).text(connection.name);
-    $('.host', connectionTemplate).text(host);
 
     let background = Color(template.background);
     let lightBackground = background.lighten(0.15).hexString();
@@ -61,14 +59,51 @@ function renderConnection(connection, template) {
     }, (event) => {
         $(tip).text('');
     });
+}
+
+function bindDeleteConnection(connectionTemplate) {
+    $(connectionTemplate).addClass('delete');
+
+    $(connectionTemplate).click(() => {
+        $(connectionTemplate).toggleClass('marked');
+    });
+
+    $('.tools', connectionTemplate).hide();
+}
+
+function renderConnection(connection, template, action) {
+    let connectionTemplate = $('.fresh-connection .connection').clone();
+    let port = connection.port;
+    let host = connection.host + (port && port != '22' ? ':' + port : '');
+
+    $(connectionTemplate).data('id', connection.id);
+    $('.name', connectionTemplate).text(connection.name);
+    $('.host', connectionTemplate).text(host);
+
+    if(action == 'delete')
+        bindDeleteConnection(connectionTemplate);
+    else
+        bindConnection(connection, connectionTemplate, template);
 
     $('.my-connections-page .connections').append(connectionTemplate);
 }
 
-function renderConnections(settings) {
+function renderConnections(settings, action='normal') {
     $('.my-connections-page .connections').empty();
     $(settings.connections).each((index, connection) => {
-        renderConnection(connection, getTemplate(settings.templates, connection.template));
+        renderConnection(connection, getTemplate(settings.templates, connection.template), action);
+    });
+}
+
+function deleteConnections() {
+    let connections = [];
+    $('.my-connections-page .connections .connection.delete.marked').each((i, connection) => {
+        connections.push($(connection).data('id'));
+    });
+
+    ipcRenderer.send('delete-connections', connections);
+    ipcRenderer.once('deleted-connections', (event, settings) => {
+        $(document).trigger('on-connection-saved', settings);
     });
 }
 
@@ -78,9 +113,16 @@ module.exports = (settings) => {
 
     $('.my-connections-page .delete').click(() => {
         actionBar.showBar('.my-connections-page', '.confirm');
+        renderConnections(settings, 'delete');
     });
 
-    $('.my-connections-page .confirm').click(() => {
+    $('.my-connections-page .actions .confirm').click(() => {
         actionBar.hideBar('.my-connections-page');
+        deleteConnections();
+    });
+
+    $('.my-connections-page .actions .cancel').click(() => {
+        actionBar.hideBar('.my-connections-page');
+        renderConnections(settings);
     });
 };
